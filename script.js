45f912ceda195fd8e849e55093ab238281ce08c9
Abandoned Cart <twistedumbrella@gmail.com>
2021-11-08 23:38:38 -0500

    Restore identification bytes to bin files

--- script.js
+++ script.js
@@ -15,7 +15,7 @@
                 var name = ami.name;
                 var keytext = key.padStart(16, '0');
                 var keylink = key.substring(2).padStart(16, '0');
-                
+
                 var link = amiiboAPI + "images/icon_" + keylink.substr(0, 8) + "-" + keylink.substr(8, 8) + ".png"
                 var image = `<div class="amiibo-image"><img src="${link}" /></div>`;
                 t.row.add([image, `<span class="table-text">${name}</span>`, `<span class="table-text">${keytext}</span>`]);
@@ -27,11 +27,11 @@
 
     function getRandomBytes(count) {
         var output = new Uint8Array(count);
-    
+
         for (var i = 0; i < output.length; i++) {
             output[i] = Math.round(Math.random() * 255);
         }
-    
+
         return output;
     }
 
@@ -39,31 +39,34 @@
         var arr = new Uint8Array(540);
         // Set UID
         arr.set([0x04, 0x01, 0x02, 0x8F, 0x03, 0x04, 0x05, 0x06], 0x1D4);
-        
+
         // Set BCC, Internal, Static Lock, and CC
         arr.set([0x04, 0x48, 0x0F, 0xE0, 0xF1, 0x10, 0xFF, 0xEE], 0x0);
-        
+
         // Set 0xA5, Write Counter, and Unknown
         arr.set([0xA5, 0x00, 0x00, 0x00], 0x28);
-        
+
         // Set Dynamic Lock, and RFUI
         arr.set([0x01, 0x00, 0x0F, 0xBD], 0x208);
-        
+
         // Set CFG0
         arr.set([0x00, 0x00, 0x00, 0x04], 0x20C);
-        
+
         // Set CFG1
         arr.set([0x5F, 0x00, 0x00, 0x00], 0x210);
-        
+
         // Set Keygen Salt
         arr.set(getRandomBytes(32), 0x1E8);
-        
+
         // write key/amiibo num in big endian as a 64 bit value starting from offset off
         var off = 0x1DC;
         id = id.substring(2);
 
-        for(var i = 0; i < 16; i += 2, off += 1) {
-            arr[off] = parseInt(id.substring(i, i + 2), 16);
+        // write identification block
+        for(var i = 0, off1 = 84, off2 = 0x1DC; i < 16; i += 2, off1 += 1, off2 += 1) {
+            var currByte = parseInt(id.substring(i, i + 2), 16);
+            arr[off1] = currByte
+            arr[off2] = currByte
         }
 
         return arr;
@@ -89,7 +92,7 @@
             specialCharacters.forEach(function(char) {
                 file = file.replace(char, "_");
             });
-            
+
             var folder = zip.folder(ami.series);
             folder.file(file, generateData(key))
         })
@@ -107,10 +110,10 @@
 
     // Run on page load
     $(function() {
-        populateTable(); 
+        populateTable();
         oTable = $('#dataTable').DataTable({
             "lengthMenu": [[10, 25, 50, 100, -1], [10, 25, 50, 100, "All"]],
-        }); 
+        });
 
         $('#dataTable tbody').on('click', 'tr', function() {
             var data = oTable.row( this ).data();
@@ -118,7 +121,7 @@
         });
 
         $('#input').keyup(function() {
-            oTable.search(jQuery.fn.DataTable.ext.type.search.string($(this).val())).draw();  
+            oTable.search(jQuery.fn.DataTable.ext.type.search.string($(this).val())).draw();
         })
     });
 })();
